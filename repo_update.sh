ea98e76125a4780d5a38289ca7af1323930c1f6d
Rahif M <rahifmanjatha372@gmail.com>
2021-07-02 23:32:58 +0530

    Update repo_update.sh
    Remove unshallow
    Update commit ids
    Add depth and j to fetch

--- repo_update.sh
+++ repo_update.sh
@@ -38,51 +38,42 @@ enter_aosp_dir() {
 #fi
 
 enter_aosp_dir build/make
-git fetch aospa --unshallow
-git fetch https://github.com/AOSP-whatever/platform_build_make staging/pa-ruby
-git cherry-pick a241c651e32e1e900d40879214b2f9a5692c9ea5
-git cherry-pick d5381f2628ea2a038b72d3a7fdb38b16934e7ce1^..7b527b3ba90a80a07b1a0ea94b0e3926d9d31d3d
+git fetch https://github.com/AOSP-whatever/platform_build_make staging/pa-ruby --depth=4 -j16
+git cherry-pick 15ad9594e86db6c92b02d32011435e5617613577^..18ed0c3c8c7b1a4525d7c508d7f68c58c43a3706
 popd
 
 enter_aosp_dir device/qcom/common
-git fetch aospa --unshallow
-git fetch https://github.com/AOSP-whatever/android_device_qcom_common staging/pa-ruby
+git fetch https://github.com/AOSP-whatever/android_device_qcom_common staging/pa-ruby --depth=5 -j16
 git cherry-pick 7c4c1e7b79068cb891065c7005b48102ffb6c8ba^..fdad8aea5c29744b88390677d1e072441c4dda06
 popd
 
 enter_aosp_dir frameworks/av
-git fetch aospa --unshallow
-git fetch https://github.com/AOSP-whatever/platform_frameworks_av staging/pa-ruby
-git cherry-pick 11d866ca1ce7e53c2b886187960afd6f6564153e^..343b21b7ce805b9773b0d00838c5018302cfcfd9
+git fetch https://github.com/AOSP-whatever/platform_frameworks_av staging/pa-ruby --depth=8 -j16
+git cherry-pick 1d39594f18cd0bd4d0aac1842df6bde032db35cf^..4d43b958f731009444ef63f542169659b092723d
 popd
 
 enter_aosp_dir frameworks/base
-git fetch aospa --unshallow
-git fetch https://github.com/AOSP-whatever/platform_frameworks_base staging/pa-ruby
+git fetch https://github.com/AOSP-whatever/platform_frameworks_base staging/pa-ruby --depth=7 -j16
 git cherry-pick 55279ffbf31010471b67ef9db55721aaca5831a4^..1b1c84cba88d8efd36a6a2d85b2577d506570339
 popd
 
 enter_aosp_dir frameworks/native
-git fetch aospa --unshallow
-git fetch https://github.com/AOSP-whatever/platform_frameworks_native staging/pa-ruby
-git cherry-pick afd25cdf5a895a49167cdf42af0e546ac4e00da4
+git fetch https://github.com/AOSP-whatever/platform_frameworks_native staging/pa-ruby --depth=2 -j16
+git cherry-pick f4fc51bc2506914bffe5cb1df9aacef7b1d9cb75
 popd
 
 enter_aosp_dir system/sepolicy
-git fetch aospa --unshallow
-git fetch https://github.com/AOSP-whatever/platform_system_sepolicy staging/pa-ruby
-git cherry-pick 5c36bc716c161635a6db45a41d45c9c5c2614c86
+git fetch https://github.com/AOSP-whatever/platform_system_sepolicy staging/pa-ruby --depth=2 -j16
+git cherry-pick 132e37b110838dcc8a6fa8298a07022b702670e9
 popd
 
 enter_aosp_dir vendor/pa
-git fetch aospa --unshallow
-git fetch https://github.com/AOSP-whatever/android_vendor_pa staging/pa-ruby
+git fetch https://github.com/AOSP-whatever/android_vendor_pa staging/pa-ruby --depth=11 -j16
 git cherry-pick e775309869a9a1ab91954a72350e76571a951a6c^..97b50d953ea2c533c3a8333d1a5fd9bb04756166
 popd
 
 enter_aosp_dir vendor/google/pixel
-git fetch blobs --unshallow
-git fetch https://github.com/RahifM/android_vendor_google_pixel ruby
+git fetch https://github.com/RahifM/android_vendor_google_pixel ruby --depth=2 -j16
 git cherry-pick 92dc42296c5675edd1167b13155ba8444f4fe575
 popd
 
